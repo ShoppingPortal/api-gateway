deb51bd9d8b88b4dbe7914c470db227d02a6bd3e
Ankit Tyagi <ankittya@cybage.com>
2016-02-22 21:21:16 +0530

    removed unused imports

--- src/main/java/com/cisco/microservices/gateway/service/ProductService.java
+++ src/main/java/com/cisco/microservices/gateway/service/ProductService.java
@@ -3,11 +3,8 @@ package com.cisco.microservices.gateway.service;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 @FeignClient("PRODUCTS-SERVICE")
 public interface ProductService {
