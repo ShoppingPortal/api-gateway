df95812c6c196e3d638c45fe3bfaef635cd1ccc0
Ankit Tyagi <ankittya@cybage.com>
2016-02-26 12:24:53 +0530

    changed import order as per checkstyle warning

--- src/test/java/ankit/poc/ApplicationTests.java
+++ src/test/java/ankit/poc/ApplicationTests.java
@@ -1,12 +1,13 @@
 package ankit.poc;
 
+import com.cisco.microservices.gateway.App;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.cisco.microservices.gateway.App;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = App.class)
